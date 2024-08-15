ad0c108098f9bc70af07075dec9066168f3d840c
Jagembe <58337007+Jagembe@users.noreply.github.com>
2024-08-15 05:21:22 -0400

    module1 follow along

---
+++ module1-operators.py
@@ -0,0 +1,26 @@
+print(2 + 3)
+print(5-4)
+print(3 * 5)
+
+
+# standard division always returns a float
+6/2
+
+# Integer division - always returns an integer and rounds off where the result is not whole
+7 / 2
+
+6 // 2
+
+# modulus division - always returns the remainder of the division
+6 % 2
+
+7 % 2
+
+# power operator
+2 ** 3
+
+
+
+
+
+
